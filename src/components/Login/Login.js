485cd9e7fc7ed6453ee483225f60c31e011d148f
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-04-17 16:09:59 +0600

    partial autenticarion handle

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,34 @@
 
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Login = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('');
     const navigate = useNavigate();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
 
     const submitButton = event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
+        signInWithEmailAndPassword(email, password)
     }
     const signUpNavigate = event => {
         navigate('/register')
     }
+    if(user){
+        navigate('/')
+    }
+    
     return (
         <div className='container w-50'>
             <h2 className='text-center'>Please Login!</h2>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+
 
 const Register = () => {
+    
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] =  useCreateUserWithEmailAndPassword(auth);
+ const navigate = useNavigate();
+ const emailRef = useRef('');
+ const passwordRef = useRef('');
+ const signInNavigate = ()=>{
+     navigate('/signIn')
+ }
+ const registerButton = event=>{
+     event.preventDefault();
+    //  const name = event.target.name.value;
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+     createUserWithEmailAndPassword(email, password)
+ }
+ if(user){
+     navigate('/')
+ }
     return (
         <div className='container w-50'>
               <h2 className='text-center'>Please Registration</h2>
-            <Form>
+            <Form onSubmit={registerButton}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" required />
+                    <Form.Control  ref={emailRef} type="email" placeholder="Enter email" required />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -16,18 +43,19 @@ const Register = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control  type="password" placeholder="Password" required />
+                    <Form.Control  ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
                  
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                {/* <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Confirm Password</Form.Label>
                     <Form.Control  type="password" placeholder="Confirm Password" required />
-                </Form.Group>
+                </Form.Group> */}
                 
                 <Button variant="primary" type="submit">
                     Registration
                 </Button>
             </Form> 
+            <p>Already Registered? <Link to={'/signIn'} className='text-danger pe-auto text-decoration-none' onClick={signInNavigate}>Go to Login.</Link></p>
         </div>
     );
 };
